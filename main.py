bfe054148e9d409af882e3fac1adc301869712fe
paigeuppal <sc22pu@leeds.ac.uk>
2026-03-10 13:17:30 +0000

    split out database and models for modularity - needs tidying

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi_mcp import FastApiMCP
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base, sessionmaker, Session
+#from sqlalchemy import create_engine, Column, Integer, String, Float
+#from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from typing import Optional
 
+#from fastapi import FastAPI, HTTPException, Depends
+#from fastapi_mcp import FastApiMCP
+from sqlalchemy.orm import Session
 
-# Connects to blockbuster.db 
-SQLALCHEMY_DATABASE_URL = "sqlite:///blockbuster.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+from database import get_db, Base, Movie, InflationRate
 
-# Redefine models so FastAPI knows how to read the tables
-class Movie(Base):
-    __tablename__ = 'movies'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    release_year = Column(Integer, nullable=False)
-    budget = Column(Float, nullable=False)
-    revenue = Column(Float, nullable=False)
+# # Connects to blockbuster.db 
+# SQLALCHEMY_DATABASE_URL = "sqlite:///blockbuster.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
-class InflationRate(Base):
-    __tablename__ = 'inflation_rates'
-    year = Column(Integer, primary_key=True)
-    cpi = Column(Float, nullable=False)
+# # Redefine models so FastAPI knows how to read the tables
+# class Movie(Base):
+#     __tablename__ = 'movies'
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String, nullable=False)
+#     release_year = Column(Integer, nullable=False)
+#     budget = Column(Float, nullable=False)
+#     revenue = Column(Float, nullable=False)
 
-# Dependency to safely open and close a database connection for each user request
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# class InflationRate(Base):
+#     __tablename__ = 'inflation_rates'
+#     year = Column(Integer, primary_key=True)
+#     cpi = Column(Float, nullable=False)
+
+# # Dependency to safely open and close a database connection for each user request
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 # pydantic model for the API response - ensures consistent output format and validation
 class MovieAdjustedResponse(BaseModel):
